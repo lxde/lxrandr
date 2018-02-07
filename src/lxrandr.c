e20e288aa9c0c655cf2c5bc6f5b7e6d1c1599173
Mamoru TASAKA <mtasaka@fedoraproject.org>
2018-02-07 15:16:21 +0900

    Allow dot on monitor name
    With some monitor now xrandr seems to return something like
      DP-0.9 connected 1920x2160+0+0 (normal left inverted right x axis y axis) 698mm x 392mm
    with containing dot in monitor name.
    
    Let lxrandr allow this

--- src/lxrandr.c
+++ src/lxrandr.c
@@ -118,7 +118,7 @@ static gboolean get_xrandr_info()
         return FALSE;
     }
 
-    regex = g_regex_new( "\n([-a-zA-Z]+[-0-9]*) +connected ([^(\n ]*)[^\n]*"
+    regex = g_regex_new( "\n([-\\.a-zA-Z]+[-\\.0-9]*) +connected ([^(\n ]*)[^\n]*"
                          "((\n +[0-9]+x[0-9]+[^\n]+)+)",
                          0, 0, NULL );
     if( g_regex_match( regex, output, 0, &match ) )
